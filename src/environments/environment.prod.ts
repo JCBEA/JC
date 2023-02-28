d0ffa7f3f8d3cae47fcafa12b25757a391adcfe6
JCBEA <111287289+JCBEA@users.noreply.github.com>
2023-02-28 17:42:20 +0800

    new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'secondsem-fd19a',
-    appId: '1:148011215025:web:74b13939029430c3405a90',
-    databaseURL: 'https://secondsem-fd19a-default-rtdb.firebaseio.com',
-    storageBucket: 'secondsem-fd19a.appspot.com',
-    apiKey: 'AIzaSyCaGd_ldX1CO87Rhp0ZF1oYJ5u26VJ5AJc',
-    authDomain: 'secondsem-fd19a.firebaseapp.com',
-    messagingSenderId: '148011215025',
+    projectId: 'newsemsecond',
+    appId: '1:537573845279:web:f0cfddc853f82a659a282a',
+    databaseURL: 'https://newsemsecond-default-rtdb.firebaseio.com',
+    storageBucket: 'newsemsecond.appspot.com',
+    apiKey: 'AIzaSyCMCG30P7AatLjh4y_LNRPnfaxPWF9r2aE',
+    authDomain: 'newsemsecond.firebaseapp.com',
+    messagingSenderId: '537573845279',
   },
   production: true
 };
